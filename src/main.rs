08f9fc227388f9696d3f12541d967934b64c8343
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2022-04-07 23:20:53 -0700

    release: 0.0.9
    Fully functioning UI with images and toggles. Polish will be added this weekend.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@
 #![feature(drain_filter)]
 
 #[cfg(not(target_arch = "wasm32"))]
-use dorothy::{app::AppDorothy};
-use eframe::epi::{IconData};
+use dorothy::app::AppDorothy;
+use eframe::epi::IconData;
 
 fn main() -> Result<(), confy::ConfyError> {
-
-    let dorothy_icon: Vec<u8> = image::open("C:/Github/Dorothy-egui/src/images/dorothy.png").unwrap().into_rgba8().to_vec();
-    let app_icon: IconData = IconData { rgba: dorothy_icon, width: 32, height: 32 };
+    let dorothy_icon: Vec<u8> = image::open("C:/Github/Dorothy-egui/src/images/dorothy.png")
+        .unwrap()
+        .into_rgba8()
+        .to_vec();
+    let app_icon: IconData = IconData {
+        rgba: dorothy_icon,
+        width: 32,
+        height: 32,
+    };
 
     let app = AppDorothy::new();
     let native_options = eframe::NativeOptions {
